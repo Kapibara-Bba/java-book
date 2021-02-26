effe324c0a99e9f7512ae96cbaf6963b4eb265c5
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-02-26 21:22:18 +0900

    [Add]Kantan

--- Aisatsu.java
+++ Aisatsu.java
@@ -1,7 +1,5 @@
 public class Aisatsu {
   public static void main(String[] args) {
-    System.out.println("おはよう");
-    System.out.println("こんにちは");
-    System.out.println("こんばんは");
+    System.out.println("おはよう。 \nこんにちは。 \nこんばんは。");
   }
 }
