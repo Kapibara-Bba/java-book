3a2adc652ed6487b8d209494f2aaae89b434b196
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-03-04 17:09:26 +0900

    [Add]DrawGraph3

---
+++ DrawGraph3.java
@@ -0,0 +1,12 @@
+public class DrawGraph3 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.print(i + ":");
+      //for (int j = 0; j < i; i++) {  i++のところが間違い  j++となる
+        for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
