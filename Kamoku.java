2102d0f31124075d7b742bad439a53afe04abc2d
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-03-12 15:41:49 +0900

    [Add]Kamoku

---
+++ Kamoku.java
@@ -0,0 +1,17 @@
+// 科目を表すクラス（科目と点数の組）
+public class Kamoku {
+  String namae; //科目名
+  int tensuu;   //点数
+
+  //科目の作成
+  public Kamoku(String namae, int tensuu) {
+    this.namae = namae;
+    this.tensuu = tensuu;
+  }
+
+  //科目の文字列表現
+  @Override
+  public String toString() {
+    return namae + "は" + tensuu + "点";
+  }
+}
